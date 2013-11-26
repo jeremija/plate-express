54b7c073f7f5aa02d26afd9418bf66e56f9e6ea7
jeremija <jerko.steiner@gmail.com>
2013-11-26 09:52:08 +0100

    Added grunt-mocha-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,32 +12,32 @@ module.exports = function(grunt) {
         //         }
         //     }
         // },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/test.js']
+            }
+        },
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint', 'mocha']
         },
     });
 
-    grunt.registerTask('mocha', 'run mocha tests', function() {
-        var done = this.async();
-        require('child_process').exec('./node_modules/.bin/mocha ./test/test.js',
-            function(err, stdout) {
-                grunt.log.write(stdout);
-                done(err);
-            });
-    });
-
     grunt.registerTask('start', 'start application', function() {
         var done = this.async();
         require('./src/index.js');
-        // do not call done so you havo to terminate manually
+        // do not call done() to make the server keep running
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-test');
     // grunt.loadNpmTasks('grunt-jsdoc');
 
-    grunt.registerTask('test', ['jshint', 'mocha']);
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
     // grunt.registerTask('doc', ['jshint', 'mochaTest', 'jsdoc']);
-    grunt.registerTask('default', ['jshint', 'mocha']);
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
 };
